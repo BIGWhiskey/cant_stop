7f515acef65c6634c0dbc62a3f763162a7d5bae8
Christopher Dowd <cdowd3@unh.newhaven.edu>
2021-03-23 19:20:25 -0400

    Board class created

---
+++ src/Board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christopher Dowd on 3/23/21.
+//
+
+#include "Board.h"
---
+++ src/Board.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Christopher Dowd on 3/23/21.
+//
+
+#ifndef CANT_STOP_BOARD_HPP
+#define CANT_STOP_BOARD_HPP
+
+
+class Board {
+
+};
+
+
+#endif //CANT_STOP_BOARD_HPP
